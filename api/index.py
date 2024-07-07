d85b0cd66bed6773141026713bc6a8ace4315212
BMWdejf <sablatura.david@gmail.com>
2024-07-07 21:56:53 +0200

    Test FastAPI on Vercel

--- api/index.py
+++ api/index.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/python/products")
+def get_products():
+    return {"message": "All Products"}
